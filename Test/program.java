e8f15686090b35369c7883fb4b99ef1c36d4f46c
BackDad <manuk_1122@mail.ru>
2023-03-06 10:39:51 +0300

    Chahge some text in print

---
+++ Test/program.java
@@ -0,0 +1,12 @@
+/**
+ * program
+ */
+public class program { // snippet class
+
+    public static void main(String[] args) { //snippet main
+        System.out.println("I lo ve you baby"); // snippet sysout
+        int a =123;
+        boolean f = a <=  12;
+        System.out.println(f);
+    }
+}
